2d8c68fe887e759f4bef7b09b8419b0351cbae45
Meno Abels <meno.abels@adviser.com>
2020-02-15 19:38:04 +0100

    remove node-fetch reference

--- src/http/services/fetch.service.ts
+++ src/http/services/fetch.service.ts
@@ -1,6 +1,8 @@
 import { Inject } from '@andrei-tatar/ts-ioc';
 import { LogService } from '../../services/log-service';
 
+import fetch, {RequestInit, Response} from 'node-fetch';
+
 export class FetchService {
 
     constructor(@Inject(LogService) private log: LogService) { }
